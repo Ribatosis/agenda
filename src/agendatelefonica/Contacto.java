21dc2b3b9db7ae37b88d45fae260de62b7e9c1a6
Jhan-Paul11 <jhanpaul.bejarano@genstudents.org>
2024-06-18 11:03:13 -0500

    actualizacion jhan

--- src/agendatelefonica/Contacto.java
+++ src/agendatelefonica/Contacto.java
@@ -3,4 +3,32 @@ package agendatelefonica;
 public class Contacto {
     private String telefono;
     private String nombre;
+
+    //constructor
+    public Contacto(){
+        //contructor de default
+    }
+
+    public Contacto(String telefono, String nombre) {
+        this.telefono = telefono;
+        this.nombre = nombre;
+    }
+    //getter y setter
+
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
